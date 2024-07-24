1e34506ee9fabc195349af2b418584c515c550fb
Jay-Ubisse <joaquimubisse@gmail.com>
2024-07-24 15:26:08 +0200

    feat: DB connection

--- backend/controllers/task-controllers.js
+++ backend/controllers/task-controllers.js
@@ -1,26 +1,28 @@
-const getAllTasks = (req, res) => {
-  //CÓDIGO PARA IR BUSCAR E PROCESSAR DADOS DA BASE DE DADO
-  res.status(200).json({ message: "Obter todas as tarefas" });
-};
+const asyncHandler = require("express-async-handler");
 
-const getTask = (req, res) => {
+const getAllTasks = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Obter todas as tarefas" });
+});
+
+const getTask = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Obter tarefa ${req.params.id}`,
   });
-};
+});
 
-const createTask = (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Tarefa criada com sucesso!", data: req.body });
-};
+const createTask = asyncHandler(async (req, res) => {
+  res.status(200).json({
+    message: "Tarefa criada com sucesso!",
+    data: req.body,
+  });
+});
 
-const updateTask = (req, res) => {
+const updateTask = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Actualizar tarefa ${req.params.id}` });
-};
+});
 
-const deleteTask = (req, res) => {
+const deleteTask = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Eliminar uma tarefa ${req.params.id}` });
-};
+});
 
 module.exports = { getAllTasks, getTask, deleteTask, createTask, updateTask };
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const connectDB = require("./connect/database");
+
 const router = require("./routes/task-routes");
 
 const port = process.env.PORT;
 
+connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
